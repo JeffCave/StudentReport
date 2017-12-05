82ebce81c58a3e682bc73733fc2f71fdb39dfe2b
Jeff Cave <jefferey.cave@gmail.com>
2017-12-05 02:04:47 +0000

    Updates correctly with change of student

--- DisplayGradeAttend.js
+++ DisplayGradeAttend.js
@@ -44,7 +44,7 @@ function DisplayGradeAttend(force){
         		};
         		db.query('metrics/gradesByDate', opts)
         			.then( function(result){
-        			    rates = Object.entries(result.rows.reduce(function(a,d){
+        			    rates = result.rows.reduce(function(a,d){
         			        let key = d.key[1];
         			        a[key] = a[key] || {
         			            attendance:0,
@@ -52,26 +52,46 @@ function DisplayGradeAttend(force){
         			        };
         			        a[key].grade = d.value.pct;
         			        return a;
-        			    },rates))
-        			    .map(function(d){
-        			        d[1].id = d[0];
-        			        return d[1];
+        			    },rates)
+        			    
+        			    let current = JSON.clone(rates[params.student]);
+        			    
+        			    rates = Object.entries(rates).reduce(function(a,d){
+        			        let key = JSON.stringify([
+        			                Math.round(10*d[1].grade)/10,
+        			                Math.round(10*d[1].attendance)/10
+        			            ]);
+        			        a[key] = (a[key] || 0) + 1;
+        			        return a;
+        			    },{});
+        			    rates = Object.entries(rates).map(function(d){
+        			        let rtn = JSON.parse(d[0])
+        			        rtn = {
+        			            grade: rtn[0],
+        			            attendance: rtn[1],
+        			            qty:d[1]
+        			        }
+        			        return rtn;
         			    });
         			    
         			    /******************************************/
         			    // set the dimensions and margins of the graph
-                        var margin = {top: 20, right: 20, bottom: 30, left: 50},
-                            width = 960 - margin.left - margin.right,
-                            height = 500 - margin.top - margin.bottom;
+                        let margin = {top: 30, right: 30, bottom: 30, left: 30},
+                            width = 480 - margin.left - margin.right,
+                            height = 480 - margin.top - margin.bottom;
                         
                         // set the ranges
-                        var x = d3.scaleLinear().range([0, width]);
-                        var y = d3.scaleLinear().range([height, 0]);
+                        let x = d3.scaleLinear().range([0, width]);
+                        let y = d3.scaleLinear().range([height, 0]);
                         
                         // append the svg obgect to the body of the page
                         // appends a 'group' element to 'svg'
                         // moves the 'group' element to the top left margin
-                        var svg = d3.select("#studentdata")
+                        let svg = d1.querySelector("#studentdata svg");
+                        if(svg){
+                            svg.parentNode.removeChild(svg);
+                        }
+                        svg = d3.select("#studentdata")
                             .append("svg")
                                 .attr("width", width + margin.left + margin.right)
                                 .attr("height", height + margin.top + margin.bottom)
@@ -82,43 +102,6 @@ function DisplayGradeAttend(force){
                         x.domain([0,1]);
                         y.domain([0,1]);
                         
-                        // Add the scatterplot
-                        svg.selectAll("circle")
-                            .data(rates)
-                            .enter()
-                                .append("circle")
-                                  .attr("r", function(d){
-                                      //return (params.student == d.id) ? '6' : '5';
-                                      return 6;
-                                  })
-                                  .attr("cx", function(d) {
-                                      return x(d.grade); 
-                                  })
-                                  .attr("cy", function(d) {
-                                      return y(d.attendance); 
-                                  })
-                                  .attr("fill", function(d) {
-                                      let color = 'black';
-                                      if(params.student == d.id){
-                                          color = 'dodgerblue';
-                                      }
-                                      return color; 
-                                  })
-                                  .attr("fill-opacity",function(d){
-                                      let color = 0.5;
-                                      if(params.student == d.id){
-                                          color = 1;
-                                      }
-                                      return color;
-                                  })
-                                  .attr('stroke',function(d){
-                                      return (params.student == d.id) ? 'black' : '';
-                                  })
-                                  .attr('stroke-width',function(d){
-                                      return (params.student == d.id) ? '2' : '';
-                                  })
-                                  ;
-                    
                         // Add the X Axis
                         svg.append("g")
                             .attr("transform", "translate(0," + height + ")")
@@ -128,6 +111,36 @@ function DisplayGradeAttend(force){
                         svg.append("g")
                             .call(d3.axisLeft(y));
                         
+                        // Add the scatterplot
+                        svg.selectAll("circle")
+                            .data(rates)
+                            .enter()
+                                .append("circle")
+                                  .attr("r",function(d){
+                                      return 3+(d.qty*2);
+                                  })
+                                  .attr("cx", function(d) {
+                                      return x(d.attendance); 
+                                  })
+                                  .attr("cy", function(d) {
+                                      return y(d.grade); 
+                                  })
+                                  .attr("style","fill:#888888;fill-opacity:1")
+                                  ;
+                        let curMarker = svg.selectAll("ellipse")
+                            .data([current])
+                            ;
+                        curMarker
+                            .enter()
+                                .append("ellipse")
+                                    .attr("rx",6)
+                                    .attr("ry",6)
+                                    .attr("style",'fill:dodgerblue;stroke:black;stroke-width:1;')
+                            .merge(curMarker)
+                                .attr("cx", x(Math.round(10*current.attendance)/10))
+                                .attr("cy", y(Math.round(10*current.grade)/10))
+                                ;
+                    
         			    /******************************************/
         			    
         			    
